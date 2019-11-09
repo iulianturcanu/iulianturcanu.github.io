7fdc1c4cc182864b6679f56786f2471774faabe4
Iulian Turcanu <iudturcanu@gmail.com>
2019-11-08 21:44:34 -0500

    site v2

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+function makeVisible(){
+    var x = document.getElementById("about");
+    if(x.style.display === "none" || x.style.display =="") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
