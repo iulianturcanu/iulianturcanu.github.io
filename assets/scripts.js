cf22cf9708387720b4915c5263880b6fedf56af5
iuianturcanu <iudturcanu@gmail.com>
2021-01-05 13:21:41 -0500

    includedFooter added

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,6 @@
-function makeVisible(){
-    var x = document.getElementById("about");
-    if(x.style.display === "none" || x.style.display =="") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
 
 $(document).ready(function() {
   $("#includedHeader").load("header.html");
+  $("#includedFooter").load("footer.html");
+
 });
